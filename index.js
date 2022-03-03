f5047339e38ae118fffad7ce0bc85379d045684b
yoooung2 <92067990+yoooung2@users.noreply.github.com>
2022-03-03 14:56:39 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(window).on('scroll', function(){
   if(window.scrollY >= 200 && window.scrollY < 700) {
     $('.center').css({'z-index':'auto'});
-  } else if(window.scrollY >= 3050 && window.scrollY < 3550){
+  } else if(window.scrollY >= 3500 && window.scrollY < 4000){
     $('.center').css({'z-index':'auto'});
   }else {
     $('.center').css({'z-index':'99'});
