071f26c7e5140685f27ce714863acf1c3848f426
yoooung2 <92067990+yoooung2@users.noreply.github.com>
2022-03-14 12:53:46 +0900

    ㅡㅡ

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(window).on('scroll', function(){
   if(window.scrollY >= 160 && window.scrollY < 700) {
     $('.center').css({'z-index':'auto'});
-  } else if(window.scrollY >= 3500 && window.scrollY < 4000){
+  } else if(window.scrollY >= 3000 && window.scrollY < 3500){
     $('.center').css({'z-index':'auto'});
   }else {
     $('.center').css({'z-index':'99'});
